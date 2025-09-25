6da58f2e555389ab951bdec0c5c7cc5e20841b09
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-09-25 17:17:11 +0530

    add theme

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	sassOptions: {
+		additionalData: `@use '@/common/styles/theme.scss' as *;`,
+	},
 };
 
 export default nextConfig;
