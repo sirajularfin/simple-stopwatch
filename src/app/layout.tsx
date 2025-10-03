f0ebfacf56a347420186529fe4d03723e3b7666a
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-10-03 21:12:20 +0530

    refactor code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import { Analytics } from '@vercel/analytics/next';
 import classNames from 'classnames';
 import type { Metadata } from 'next';
-import { NextIntlClientProvider } from 'next-intl';
 import { getLocale } from 'next-intl/server';
-import { cookies } from 'next/headers';
 
 import { montserratFonts, russoOneFonts } from '@/../public/fonts/fonts';
 import '@/common/styles/globals.scss';
-import { APP_LANGUAGES } from '@/common/types/constants';
 import MainHeader from '@/components/MainHeader/MainHeader';
-import LocalizationProvider from '@/i18n/LocalizationProvider';
+import { AppProviders } from '@/contexts/AppProviders';
 
 export const metadata: Metadata = {
 	title: 'Stopwatch App',
@@ -22,21 +19,15 @@ export default async function RootLayout({
 	children: React.ReactNode;
 }>) {
 	const locale = await getLocale();
-	const cookieStore = await cookies();
-	const cookieLocale = cookieStore.get('locale')?.value as APP_LANGUAGES;
-	const initialLocale =
-		cookieLocale && Object.values(APP_LANGUAGES).includes(cookieLocale) ? cookieLocale : APP_LANGUAGES.EN;
 
 	return (
 		<html lang={locale}>
 			<body className={classNames(russoOneFonts.variable, montserratFonts.variable)}>
 				<Analytics />
-				<LocalizationProvider initialLocale={initialLocale}>
-					<NextIntlClientProvider locale={locale}>
-						<MainHeader />
-						<main>{children}</main>
-					</NextIntlClientProvider>
-				</LocalizationProvider>
+				<AppProviders>
+					<MainHeader />
+					<main>{children}</main>
+				</AppProviders>
 			</body>
 		</html>
 	);
