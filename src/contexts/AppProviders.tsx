4e5ecb1dd068a9f1f5720d9fbe5b62550e6834da
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-10-03 22:55:15 +0530

    Add context and other assets

---
+++ src/contexts/AppProviders.tsx
@@ -0,0 +1,27 @@
+import { NextIntlClientProvider } from 'next-intl';
+import { getLocale } from 'next-intl/server';
+import { cookies } from 'next/headers';
+import React from 'react';
+
+import { APP_LANGUAGES } from '@/common/types/constants';
+import LocalizationProvider from '@/i18n/LocalizationProvider';
+import { StopwatchProvider } from './stopwatch/StopwatchProvider';
+import { TimerProvider } from './timer/TimerProvider';
+
+export const AppProviders: React.FC<React.PropsWithChildren> = async ({ children }) => {
+	const locale = await getLocale();
+	const cookieStore = await cookies();
+	const cookieLocale = cookieStore.get('locale')?.value as APP_LANGUAGES;
+	const initialLocale =
+		cookieLocale && Object.values(APP_LANGUAGES).includes(cookieLocale) ? cookieLocale : APP_LANGUAGES.EN;
+
+	return (
+		<LocalizationProvider initialLocale={initialLocale}>
+			<NextIntlClientProvider locale={locale}>
+				<StopwatchProvider>
+					<TimerProvider>{children}</TimerProvider>
+				</StopwatchProvider>
+			</NextIntlClientProvider>
+		</LocalizationProvider>
+	);
+};
---
+++ src/contexts/timer/types.ts
@@ -0,0 +1,11 @@
+export interface ITimerState {
+	running: boolean;
+	elapsedMs: number;
+}
+
+export interface ITimerContextProps extends ITimerState {
+	start: () => void;
+	pause: () => void;
+	stop: () => void;
+	reset: () => void;
+}
