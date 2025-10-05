5e89a202f38009e75d8b5d98a5d981303e0ebb44
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-10-05 19:26:27 +0530

    feat: update main header component

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -1,18 +1,30 @@
 import { getTranslations } from 'next-intl/server';
+import Link from 'next/link';
 import React from 'react';
 
+import { APP_ROUTES } from '@/common/types/app.routes';
 import Typography from '../Typography/Typography';
 import classes from './style.module.scss';
+
+const HEADER_NAVIGATION_LINKS = [
+  { href: APP_ROUTES.STOPWATCH, labelKey: 'headerItems_stopwatch' },
+  { href: APP_ROUTES.TIMER, labelKey: 'headerItems_timer' },
+];
 
 const MainHeader: React.FC = async () => {
   const t = await getTranslations();
 
   return (
     <header className={classes.container}>
-      <ul>
-        <Typography variant="listItem">{t('headerItems_stopwatch')}</Typography>
-        <Typography variant="listItem">{t('headerItems_timer')}</Typography>
-      </ul>
+      <nav className={classes.navigation}>
+        <ul className={classes.list}>
+          {HEADER_NAVIGATION_LINKS.map(({ href, labelKey }) => (
+            <Typography variant="listItem" key={href}>
+              <Link href={href}>{t(labelKey)}</Link>
+            </Typography>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 };
