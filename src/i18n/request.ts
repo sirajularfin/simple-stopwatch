bbff3af09ee9e54c73809c383ee7126e3dd5164e
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-10-05 22:47:46 +0530

    Fix translation issue

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -3,9 +3,11 @@ import { cookies } from 'next/headers';
 
 import { APP_LANGUAGES } from '@/common/types/constants';
 
-const resources: Record<APP_LANGUAGES, () => Promise<Record<string, string>>> = {
-	[APP_LANGUAGES.EN]: () => import('../translations/en.json').then((m) => m.default),
-};
+const resources: Record<APP_LANGUAGES, () => Promise<Record<string, unknown>>> =
+  {
+    [APP_LANGUAGES.EN]: () =>
+      import('@/translations/en.json').then(m => m.default),
+  };
 
 export default getRequestConfig(async () => {
 	const cookieStore = await cookies();
