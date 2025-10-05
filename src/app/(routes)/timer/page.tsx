5cf6891f59dfc06a5e16b09233817b06acf7ac7e
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-10-05 19:57:28 +0530

    feat: fix layout

--- src/app/(routes)/timer/page.tsx
+++ src/app/(routes)/timer/page.tsx
@@ -1,11 +1,6 @@
 import { Metadata } from 'next';
-import { getTranslations } from 'next-intl/server';
 
-import { ACTION_TYPES, APPLICATION_MODES } from '@/common/types/constants';
-import ActionButton from '@/components/ActionButton/ActionButton';
-import DisplayTimer from '@/components/DisplayTimer/DisplayTimer';
 import Preset from '@/components/Preset/Preset';
-import TextInput from '@/components/TextInput/TextInput';
 import classes from './style.module.scss';
 
 export const metadata: Metadata = {
@@ -14,20 +9,8 @@ export const metadata: Metadata = {
 };
 
 export default async function Timer() {
-  const t = await getTranslations();
-
   return (
     <div className={classes.container}>
-      <div className={classes.wrapper}>
-        <div className={classes.timerPreset}>
-          <DisplayTimer mode={APPLICATION_MODES.TIMER} />
-          <TextInput placeholder={t('savePreset_placeholder')} />
-        </div>
-        <div className={classes.actionButtons}>
-          <ActionButton type={ACTION_TYPES.PLAY} size="LARGE" />
-          <ActionButton type={ACTION_TYPES.STOP} size="LARGE" />
-        </div>
-      </div>
       <div className={classes.presets}>
         <Preset title={'Study Time'} timestamp="25:00" index={1} />
         <Preset title={'Study Time'} timestamp="25:00" index={1} />
