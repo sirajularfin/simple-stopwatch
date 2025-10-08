8945cd8d67f8c016d0148449bebad053a8ed6a2b
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-10-08 18:13:00 +0530

    feat: update metadata

--- src/app/(routes)/stopwatch/page.tsx
+++ src/app/(routes)/stopwatch/page.tsx
@@ -1,8 +1,1 @@
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Focus Timer & Stopwatch | Pomodoro Presets',
-  description: 'A simple timer application built with Next.js',
-};
-
 export default async function Stopwatch() {}
