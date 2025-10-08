e8c1a1869474c11807da2208569c0b74dffe638d
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-10-08 17:58:40 +0530

    feat: add loader

---
+++ src/app/(routes)/loading.tsx
@@ -0,0 +1,28 @@
+import classes from './style.module.scss';
+
+const COMPONENT_SIZE = 44;
+const THICKNESS = 4;
+
+const Loading = () => {
+  return (
+    <svg
+      width={COMPONENT_SIZE}
+      height={COMPONENT_SIZE}
+      viewBox="0 0 44 44"
+      className={classes.loader}
+    >
+      <circle strokeWidth={THICKNESS}>
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          from="0 22 22"
+          to="360 22 22"
+          dur="1s"
+          repeatCount="indefinite"
+        />
+      </circle>
+    </svg>
+  );
+};
+
+export default Loading;
