209487de7d78db5bf262a734ca6cc7e2a2a568d2
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-10-09 12:42:04 +0530

    feat: add stopwatch lap table and its styles

--- src/app/(routes)/stopwatch/page.tsx
+++ src/app/(routes)/stopwatch/page.tsx
@@ -1,21 +1,35 @@
 import { useTranslations } from 'next-intl';
+import classes from './style.module.scss';
 
 export default function Stopwatch() {
   const t = useTranslations('stopwatch_lap');
 
   return (
-    <table>
+    <table className={classes.table}>
       <thead>
         <tr>
-          <th scope="col">{t('lap_number')}</th>
-          <th scope="col">{t('lap_time')}</th>
-          <th scope="col">{t('split_time')}</th>
+          <th scope="col" align="left">
+            {t('lap_number')}
+          </th>
+          <th scope="col" align="center">
+            {t('lap_time')}
+          </th>
+          <th scope="col" align="right">
+            {t('split_time')}
+          </th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <th scope="row">1</th>
-          <td>00:00:01</td>
+          <td scope="row" align="left">
+            1
+          </td>
+          <td scope="row" align="center">
+            00:00:01
+          </td>
+          <td scope="row" align="right">
+            00:00:01
+          </td>
         </tr>
       </tbody>
     </table>
