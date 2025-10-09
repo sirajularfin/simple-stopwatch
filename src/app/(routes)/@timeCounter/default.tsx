d124a730be77b8097bc127a0c9266ce00a686705
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2025-10-09 22:30:49 +0530

    feat: add btn disable

--- src/app/(routes)/@timeCounter/default.tsx
+++ src/app/(routes)/@timeCounter/default.tsx
@@ -21,7 +21,11 @@ const TimeCounter: React.FC = () => {
       {state.isRunning ? (
         <ActionButton type={ACTION_TYPES.PAUSE} size="LARGE" />
       ) : (
-        <ActionButton type={ACTION_TYPES.PLAY} size="LARGE" />
+        <ActionButton
+          type={ACTION_TYPES.PLAY}
+          size="LARGE"
+          disabled={!state.elapsedMs}
+        />
       )}
       <ActionButton type={ACTION_TYPES.STOP} size="LARGE" />
     </div>
