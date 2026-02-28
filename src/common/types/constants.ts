4fc8aa29459403fa466c3ded1e1f16f56d8208bb
Sirajul Arfin <78791175+sirajularfin@users.noreply.github.com>
2026-02-28 17:44:47 +0530

    update constants

--- src/common/types/constants.ts
+++ src/common/types/constants.ts
@@ -1,3 +1,11 @@
+import {
+  DeleteIcon,
+  PauseIcon,
+  ResetIcon,
+  ResumeIcon,
+  StopIcon,
+} from '@/assets';
+
 export const DEFAULT_VALUE_ZERO = 0;
 export const TIMER_PRESET_KEY = 'TIMER_PRESETS';
 export const STOPWATCH_LAP_KEY = 'STOPWATCH_LAPS';
@@ -20,6 +28,14 @@ export enum ACTION_TYPES {
   STOP = 'STOP',
   LAP = 'LAP',
 }
+
+export const ACTION_BUTTON_ICONS = {
+  [ACTION_TYPES.PAUSE]: PauseIcon,
+  [ACTION_TYPES.PLAY]: ResumeIcon,
+  [ACTION_TYPES.DELETE]: DeleteIcon,
+  [ACTION_TYPES.STOP]: StopIcon,
+  [ACTION_TYPES.RESET]: ResetIcon,
+};
 
 export enum TIMER_PRESETS {
   ONE_MINUTE = 60_000,
